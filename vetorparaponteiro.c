e13d062fef4370aab641836247728acea3c077ef
thiago correa <thiago12812@gmail.com>
2018-03-17 00:49:32 +0000

    Create vetorparaponteiro.c

---
+++ vetorparaponteiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(){
+	
+int *p, vet[5], i;	
+	
+p= vet;
+
+printf("Digite 5 numeros\n");
+	for(i=0; i<5; i++){
+		scanf("%d", &vet[i]);
+		}
+	for(i=0; i<5; i++){
+		printf("%d\n", *(p)++);
+		}
+	
+return 0;}
