7781c076e5f8734454941058dca4af03f95cf716
thiago correa <thiago12812@gmail.com>
2018-03-17 00:56:07 +0000

    Update vetorparaponteiro.c

--- vetorparaponteiro.c
+++ vetorparaponteiro.c
@@ -3,7 +3,7 @@
 
 int main(){
 	
-int *p, vet[5], i;	
+int  vet[5], i;	
 	
 p= vet;
 
@@ -12,7 +12,34 @@ printf("Digite 5 numeros\n");
 		scanf("%d", &vet[i]);
 		}
 	for(i=0; i<5; i++){
-		printf("\n%d\n", *(p)++);
+		printf("\n%d\n", *(vet+i));
 		}
 	
 return 0;}
+
+/*
+2 forma de fazer passando de vetor para um ponteiro (sendo que vetor já e um ponteiro)
+#include <stdio.h>
+
+
+int main(){
+	
+int *p, vet[5], i;	
+	
+p= vet;
+
+printf("Digite 5 numeros\n");
+	for(i=0; i<5; i++){
+		scanf("%d", vet[i]);
+		}
+	for(i=0; i<5; i++){
+		printf("%d\n", *(p)++);
+		}
+	
+	
+	
+	
+	
+	
+	
+return 0;} */
